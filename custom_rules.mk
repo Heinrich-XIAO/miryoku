b00ad87ecf009b91b23d28ecd79cf7fce1b21fba
Heinrich Xiao <74563446+Heinrich-XIAO@users.noreply.github.com>
2026-04-06 10:23:55 -0400

    faster

--- custom_rules.mk
+++ custom_rules.mk
@@ -4,3 +4,9 @@
 # Use QWERTY for alphas and tap layer.
 MIRYOKU_ALPHAS = QWERTY
 MIRYOKU_TAP = QWERTY
+
+# Use VI-style HJKL for nav and mouse (including scroll).
+MIRYOKU_NAV = VI
+
+# Mac-style clipboard shortcuts.
+MIRYOKU_CLIPBOARD = MAC
