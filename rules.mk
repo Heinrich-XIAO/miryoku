65ca5cab8b7d8a6ee74f41e11091d9e024275b6b
Heinrich Xiao <74563446+Heinrich-XIAO@users.noreply.github.com>
2026-04-06 16:52:16 -0400

    Add CVM mode keymap with toggle on top-right key

--- rules.mk
+++ rules.mk
@@ -3,13 +3,5 @@
 
 MOUSEKEY_ENABLE = yes
 EXTRAKEY_ENABLE = yes
-AUTO_SHIFT_ENABLE = yes
-TAP_DANCE_ENABLE = yes
-CAPS_WORD_ENABLE = yes
-KEY_OVERRIDE_ENABLE = yes
-
-INTROSPECTION_KEYMAP_C = manna-harbour_miryoku.c # keymaps
 
 include keyboards/silakka54/keymaps/miryoku/custom_rules.mk
-
-include users/manna-harbour_miryoku/post_rules.mk
