31190392f84302cb6994ecab2012cf089bf41f05
Heinrich Xiao <74563446+Heinrich-XIAO@users.noreply.github.com>
2026-04-06 19:29:41 -0400

    stuff

--- keymap.c
+++ keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_SCLN, KC_4, KC_5, KC_6, KC_EQL,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_GRV, KC_1, KC_2, KC_3, KC_BSLS,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                                KC_NO, KC_DOT, KC_0,       KC_NO, KC_MINS, KC_NO
+                                KC_NO, KC_0, KC_MINS,       KC_NO, KC_MINS, KC_NO
     ),
 
     // Standard Miryoku SYM Layer (symbols on left hand)
