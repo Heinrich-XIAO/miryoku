de5154f951bc04f44176f2ca88721bcd6a8c0566
Heinrich Xiao <74563446+Heinrich-XIAO@users.noreply.github.com>
2026-04-06 17:19:05 -0400

    Add NUM and SYM layers with LT taps

--- keymap.c
+++ keymap.c
@@ -5,7 +5,9 @@
 
 enum layers {
     _CVM,
-    _BASE
+    _BASE,
+    _NUM,
+    _SYM
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -22,6 +24,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,
                                                         KC_H, LSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_SCLN), KC_QUOT,
         KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B,         KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
-                                KC_ESC, KC_SPC, KC_TAB,       KC_ENT, KC_BSPC, KC_DEL
+                                KC_ESC, KC_SPC, KC_TAB,       LT(_SYM, KC_ENT), LT(_NUM, KC_BSPC), KC_DEL
+    ),
+    [_NUM] = LAYOUT(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_7, KC_8, KC_9, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_4, KC_5, KC_6, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_1, KC_2, KC_3, KC_NO, KC_NO,
+                                KC_NO, KC_0, KC_NO,       KC_NO, KC_BSPC, KC_NO
+    ),
+    [_SYM] = LAYOUT(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_SCLN, KC_QUOT, KC_MINS, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_COMM, KC_DOT, KC_SLSH, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_LBRC, KC_RBRC, KC_BSLS, KC_NO, KC_NO,
+                                KC_NO, KC_EQL, KC_NO,       KC_NO, KC_ENT, KC_NO
     )
 };
