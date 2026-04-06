46f6221e8108514bc97ea0af26168da1d9a8cbbd
Heinrich Xiao <74563446+Heinrich-XIAO@users.noreply.github.com>
2026-04-06 11:15:59 -0400

    Map thumb keys to C,V,N,M instead of Space,Tab,Return,Backspace

--- manna-harbour_miryoku.c
+++ manna-harbour_miryoku.c
@@ -44,7 +44,7 @@
     KC_NO, L00, L01, L02, L03, L04,               R00, R01, R02, R03, R04, KC_NO, \
     KC_NO, L10, L11, L12, L13, L14,               R10, R11, R12, R13, R14, KC_NO, \
     KC_NO, L20, L21, L22, L23, L24,               R20, R21, R22, R23, R24, KC_NO, \
-                      L32, L33, L34,        R30, R31, R32 \
+                      L32, KC_C, KC_V,        KC_N, KC_M, R32 \
   )
 #endif
 
